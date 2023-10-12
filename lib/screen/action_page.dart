67b52d2409c8ec6fa86ccd271e4c4ba53deac6fe
thuanpham582002 <95533596+thuanpham582002@users.noreply.github.com>
2023-10-12 22:49:20 +0700

    add search logic

--- lib/screen/action_page.dart
+++ lib/screen/action_page.dart
@@ -7,6 +7,8 @@ import 'package:iot_dashboard/resources/repo/DataRepo.dart';
 
 import '../resources/model/ActionDataTableSrc.dart';
 import '../resources/widgets/DataTable.dart';
+import '../resources/widgets/FilterView.dart';
+import '../resources/widgets/SearchView.dart';
 
 class ActionPage extends StatefulWidget {
   const ActionPage({super.key});
@@ -16,18 +18,49 @@ class ActionPage extends StatefulWidget {
 }
 
 class _ActionPageState extends State<ActionPage> {
+  ActionDataTableSrc data = DataRepo.instance.listActionData;
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Container(
           width: double.infinity,
-          child: MyDataTable(
-            data: DataRepo.instance.listActionData,
-            header: "Actions",
-            columns: ActionDataTableSrc.getColumn(),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  FilterView(),
+                ],
+              ),
+              SearchView(
+                onSearchChanged: (value) {
+                  setState(() {
+                    data = search(value);
+                  });
+                },
+              ),
+              MyDataTable(
+                data: DataRepo.instance.listActionData,
+                header: "Actions",
+                columns: ActionDataTableSrc.getColumn(),
+              ),
+            ],
           )),
     );
+  }
+
+  ActionDataTableSrc search(String value) {
+    ActionDataTableSrc _data = ActionDataTableSrc();
+    for (var item in DataRepo.instance.listActionData.read()) {
+      if (item.id.toString().contains(value) ||
+          item.statusFan.toString().contains(value) ||
+          item.statusLed.toString().contains(value) ||
+          item.time.toString().contains(value)) {
+        _data.create(item);
+      }
+    }
+    return _data;
   }
 }
 
