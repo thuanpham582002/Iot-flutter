a5083192fa1a13620b097e322d28f09f891c8e46
thuanpham582002 <95533596+thuanpham582002@users.noreply.github.com>
2023-10-12 22:49:44 +0700

    add search logic

--- lib/screen/action_page.dart
+++ lib/screen/action_page.dart
@@ -1,6 +1,3 @@
-import 'dart:js_interop_unsafe';
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iot_dashboard/resources/repo/DataRepo.dart';
@@ -61,35 +58,5 @@ class _ActionPageState extends State<ActionPage> {
       }
     }
     return _data;
-  }
-}
-
-// The "soruce" of the table
-class MyData extends DataTableSource {
-  // Generate some made-up data
-  final List<Map<String, dynamic>> _data = List.generate(
-      200,
-      (index) => {
-            "id": index,
-            "title": "Item $index",
-            "price": Random().nextInt(10000)
-          });
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get rowCount => _data.length;
-
-  @override
-  int get selectedRowCount => 0;
-
-  @override
-  DataRow getRow(int index) {
-    return DataRow(cells: [
-      DataCell(Text(_data[index]['id'].toString())),
-      DataCell(Text(_data[index]["title"])),
-      DataCell(Text(_data[index]["price"].toString())),
-    ]);
   }
 }
